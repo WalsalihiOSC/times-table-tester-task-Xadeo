ea2a20a4b7385279f4b992e01df233198eda23d7
Tong Chan <86693694+Xadeo@users.noreply.github.com>
2022-04-06 15:01:01 +1200

    Created Timestb_TesterGUI class

--- Times_tabletester.py
+++ Times_tabletester.py
@@ -1,8 +1,24 @@
 
 from  tkinter import *
 
+class Timestb_TesterGUI:
+	def __init__(self, parent):
+		font = ("Comic Sans MS", "10", "bold")
+
+		question_label = Label(parent, text = "", font = font)
+		question_label.grid(row = 0, column = 0)
+
+		self.entrypoint = Entry(parent, width = 5)
+		self.entrypoint.grid(row = 0, column = 1)
+
+		Bt1 = Button(parent, text = "Check Answer")
+		Bt1.grid(row = 1, column = 0)
+
+		Bt2 = Button(parent, text = "Next")
+		Bt2.grid(row = 1, column = 1, padx = 10)
+
 #main routine
 if __name__ == "__main__": 
 	root = Tk()
-	timestb_tester = timestb_tester(root)
+	timestb_tester = Timestb_TesterGUI(root)
 	root.mainloop()
