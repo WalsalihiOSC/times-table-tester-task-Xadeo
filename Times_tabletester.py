b4e0755627439a72a8dcb8068075214e31d88ec0
Tong Chan <86693694+Xadeo@users.noreply.github.com>
2022-04-08 10:09:45 +1200

    Created a check_answer function in GUI Class
    Added a output label to display answer in the GUI. The check_answer function gives the Check Class the user input and return a if the input is correct or not.

--- Times_tabletester.py
+++ Times_tabletester.py
@@ -1,5 +1,4 @@
 
-from distutils.cmd import Command
 from  tkinter import *
 import random
 
@@ -12,10 +11,10 @@ class Check:
 	
 	def vaildate(self, inputanswer):
 		"""Checks if the input is valid"""
-		if int(inputanswer) == self.answer:
-			return str(inputanswer) + "is correct."
+		if int(inputanswer) == int(self.answer):
+			return str(inputanswer) + " is correct."
 		else:
-			return str(inputanswer) + "is no correct."
+			return str(inputanswer) + " is no correct."
 
 class Timestb_TesterGUI:
 	def __init__(self, parent):
@@ -28,16 +27,21 @@ class Timestb_TesterGUI:
 		self.entrypoint = Entry(parent, width = 5)
 		self.entrypoint.grid(row = 0, column = 1)
 
-		Bt1 = Button(parent, text = "Check Answer")
+		Bt1 = Button(parent, text = "Check Answer", command = self.check_answer)
 		Bt1.grid(row = 1, column = 0, sticky = SW, padx = 40)
 
 		Bt2 = Button(parent, text = "Next", command = self.next)
 		Bt2.grid(row = 1, column = 1, sticky = SE, padx = 40)
 
+		self.outputlabel = Label(parent, text = "", font = ("Comic Sans MS", "10", "bold"))
+		self.outputlabel.grid(row = 2, columnspan = 2)
+
 	def next(self):
 		check1 = Check()
 		self.question_label.configure(text = f"{str(check1.randnum)} * {str(check1.randnum1)}")
 
+	def check_answer(self):
+		self.outputlabel.configure(text = self.check.vaildate(self.entrypoint.get()))
 #main routine
 if __name__ == "__main__": 
 	root = Tk()
