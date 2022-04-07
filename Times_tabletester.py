0c17170b398749ec15afda18c58f17cd31ce4456
Tong Chan <86693694+Xadeo@users.noreply.github.com>
2022-04-08 09:54:37 +1200

    Added to Timestb_TesterGUI class
    created an instance of Check class, and added text for the label.

--- Times_tabletester.py
+++ Times_tabletester.py
@@ -1,36 +1,38 @@
 
+from distutils.cmd import Command
 from  tkinter import *
 import random
 
 class Check:
 	def __init__(self):
+		""" Import random - pick two random nums"""
 		self.randnum = (random.randint(1,20))
 		self.randnum1 = (random.randint(1,20))
 		self.answer = self.randnum * self.randnum1
 	
-	def check(self, inputanswer):
+	def vaildate(self, inputanswer):
 		"""Checks if the input is valid"""
 		if int(inputanswer) == self.answer:
-			return inputanswer + "is correct."
+			return str(inputanswer) + "is correct."
 		else:
-			return inputanswer + "is no correct"
+			return str(inputanswer) + "is no correct."
 
 class Timestb_TesterGUI:
 	def __init__(self, parent):
-		font = ("Comic Sans MS", "10", "bold")
 
-		question_label = Label(parent, text = "", font = font)
-		question_label.grid(row = 0, column = 0)
+		self.check = Check()
+
+		self.question_label = Label(parent, text = f"{str(self.check.randnum)} * {str(self.check.randnum1)}")
+		self.question_label.grid(row = 0, column = 0)
 
 		self.entrypoint = Entry(parent, width = 5)
 		self.entrypoint.grid(row = 0, column = 1)
 
 		Bt1 = Button(parent, text = "Check Answer")
-		Bt1.grid(row = 1, column = 0)
+		Bt1.grid(row = 1, column = 0, sticky = SW, padx = 40)
 
-		Bt2 = Button(parent, text = "Next")
-		Bt2.grid(row = 1, column = 1, padx = 10)
-		
+		Bt2 = Button(parent, text = "Next", command = self.next)
+		Bt2.grid(row = 1, column = 1, sticky = SE, padx = 40)
 
 #main routine
 if __name__ == "__main__": 
